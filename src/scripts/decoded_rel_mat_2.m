83f1bc60b9cd010a79b0441c69ae63161917e1c5
chadji15 <computerb3xristodoulos@gmail.com>
2023-07-21 11:13:29 +0300

    cleanup

--- src/scripts/decoded_rel_mat_2.m
+++ src/scripts/decoded_rel_mat_2.m
@@ -36,13 +36,35 @@ dist = dot(diff*rel_dec,diff,2);
 lbl = training.labels(1);
 idx = training.labels == lbl;
 
-scatter(enc_dist(~idx),dist(~idx));
+invidx = ~idx;
+idx = idx(1:1000);
+invidx = invidx(1:1000);
+
+
+scatter(enc_dist(invidx),dist(invidx));
 hold;
 scatter(enc_dist(idx),dist(idx));
 hold;
 coef = polyfit(enc_dist, dist,1);
 h = refline(coef(1), coef(2));
-h.Color = 'r';
+h.Color = 'g';
+h.LineStyle = "--";
+h.LineWidth = 2;
 
+xlabel('Encoded Distance')
+ylabel('Decoded Distance')
 
+hold;
+coef2 = polyfit(enc_dist, dist,2);
+x = linspace(min(enc_dist), max(enc_dist));
+p = plot(x, polyval(coef2,x), "--magenta");
+p.LineWidth = 2;
+
+pred_dist = polyval(coef,enc_dist); 
+pred_dist2 = polyval(coef2,enc_dist); 
+
+disp("Linear model RMSE") 
+rmse1 = rmse(pred_dist, dist)
+disp("Parabolic model RMSE") 
+rmse2 = rmse(pred_dist2, dist)
     
